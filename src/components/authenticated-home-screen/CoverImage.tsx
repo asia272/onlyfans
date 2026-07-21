6cab64bbcd75e19efa67093993fbc0d4d6455b4c
asia272 <asiaashraf7272@gmail.com>
2026-07-21 13:54:41 +0500

    add coverImage

--- src/components/authenticated-home-screen/CoverImage.tsx
+++ src/components/authenticated-home-screen/CoverImage.tsx
@@ -1,5 +1,6 @@
+import { Heart, ImageIcon, Video } from 'lucide-react'
 import Image from 'next/image'
-import React from 'react'
+
 
 const CoverImage = () => {
     return (
@@ -8,8 +9,39 @@ const CoverImage = () => {
                 src={"/featured/featured10.jpg"}
                 className='h-full w-full object-cover select-none pointer-events-none'
                 fill
-                alt='horse Cover Image'
+                alt='Horse Cover Image'
             />
+            <div
+                className='absolute top-0 left-0 w-full h-full bg-gradient-to-b from-slate-800 to-transparent'
+                aria-hidden='true'
+            />
+
+            <div className='flex justify-between items-center absolute top-0 left-0 px-2 py-1 z-20 w-full'>
+                <div className='flex items-center gap-2'>
+                    <div className='flex flex-col text-white'>
+                        <p className='font-bold'>admine name</p>
+
+                        <div className='flex gap-2 items-center'>
+                            <div className='flex items-center gap-1'>
+                                <ImageIcon className='w-4 h-4' />
+                                <span className='text-sm font-bold'>image content</span>
+                            </div>
+
+                            <span className='text-xs'>•</span>
+                            <div className='flex items-center gap-1'>
+                                <Video className='w-4 h-4' />
+                                <span className='text-sm font-bold'>45</span>
+                            </div>
+
+                            <span className='text-xs'>•</span>
+                            <div className='flex items-center gap-1'>
+                                <Heart className='w-4 h-4' />
+                                <span className='text-sm font-bold'>45</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
