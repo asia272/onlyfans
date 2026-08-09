ee794ff5c097baffffc26eb22810d3e24c723a78
asia272 <asiaashraf7272@gmail.com>
2026-08-09 11:12:11 +0500

    working on one time payment

--- src/app/merch/[id]/ProductCheckout.tsx
+++ src/app/merch/[id]/ProductCheckout.tsx
@@ -5,9 +5,47 @@ import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 
+import { Product } from "@prisma/client";
+import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+import { createCheckoutSessionAction } from "./actions";
+import { useRouter } from "next/navigation";
+import { useToast } from "@/hooks/use-toast";
 
-const ProductCheckout = ({ product }: { product: any }) => {
-    const isPending = true;
+const ProductCheckout = ({ product }: { product: Product }) => {
+    const [selectedSize, setSelectedSize] = useState<string | null>(null);
+    const { toast } = useToast();
+    const router = useRouter();
+
+    const { mutate: createCheckoutSession, isPending } = useMutation({
+        mutationKey: ["createCheckoutSession"],
+        mutationFn: createCheckoutSessionAction,
+        onSuccess: ({ url }) => {
+            if (url) router.push(url);
+            else throw new Error("Error creating checkout session.Please try again later");
+        },
+        onError: (error) => {
+            toast({
+                title: "Error",
+                description: error.message || "Something went wrong. Please try again later.",
+                variant: "destructive",
+            });
+        },
+    });
+
+    const handleBuyProduct = async () => {
+        if (!selectedSize) {
+            toast({
+                title: "Error",
+                description: "Please select a size",
+                variant: "destructive",
+            });
+            return;
+        }
+        // call our mutation
+        createCheckoutSession({ productId: product.id, size: selectedSize });
+    };
+
     return (
         <div className='flex flex-col md:flex-row gap-5'>
             <ZoomedImage imgSrc={product.image} />
@@ -17,7 +55,7 @@ const ProductCheckout = ({ product }: { product: any }) => {
                 <p className='text-muted-foreground text-base'>${product.price}</p>
                 <Label className='mt-5 inline-block'>Size</Label>
 
-                <Select>
+                <Select onValueChange={setSelectedSize}>
                     <SelectTrigger className='w-[180px] focus:ring-0'>
                         <SelectValue placeholder='Select' />
                     </SelectTrigger>
@@ -32,12 +70,12 @@ const ProductCheckout = ({ product }: { product: any }) => {
                     className='mt-5 text-white px-5 py-2 rounded-md'
                     disabled={isPending}
                     size={"sm"}
+                    onClick={handleBuyProduct}
                 >
                     {isPending ? "Processing..." : "Buy Now"}
                 </Button>
             </div>
         </div>
-    )
-}
-
-export default ProductCheckout
+    );
+};
+export default ProductCheckout;
