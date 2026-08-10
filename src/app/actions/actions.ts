586a3bc202cda0b3ae382eb120c63a39496b46a9
asia272 <asiaashraf7272@gmail.com>
2026-08-10 16:49:31 +0500

    add analytics

--- src/app/actions/actions.ts
+++ src/app/actions/actions.ts
@@ -100,78 +100,78 @@ export async function toggleProductArchiveAction(productId: string) {
     return { success: true, product: updatedProduct };
 }
 
-// export async function getDashboardData() {
-//     const totalRevenuePromise = Promise.all([
-//         prisma.order.aggregate({
-//             _sum: {
-//                 price: true,
-//             },
-//         }),
-//         prisma.subscription.aggregate({
-//             _sum: {
-//                 price: true,
-//             },
-//         }),
-//     ]);
+export async function getDashboardData() {
+    const totalRevenuePromise = Promise.all([
+        prisma.order.aggregate({
+            _sum: {
+                price: true,
+            },
+        }),
+        prisma.subscription.aggregate({
+            _sum: {
+                price: true,
+            },
+        }),
+    ]);
 
-//     const totalSalesPromise = prisma.order.count();
-//     const totalSubscriptionsPromise = prisma.subscription.count();
+    const totalSalesPromise = prisma.order.count();
+    const totalSubscriptionsPromise = prisma.subscription.count();
 
-//     const recentSalesPromise = prisma.order.findMany({
-//         take: 4,
-//         orderBy: {
-//             orderDate: "desc",
-//         },
-//         select: {
-//             user: {
-//                 select: {
-//                     name: true,
-//                     email: true,
-//                     image: true,
-//                 },
-//             },
-//             price: true,
-//             orderDate: true,
-//         },
-//     });
+    const recentSalesPromise = prisma.order.findMany({
+        take: 4,
+        orderBy: {
+            orderDate: "desc",
+        },
+        select: {
+            user: {
+                select: {
+                    name: true,
+                    email: true,
+                    image: true,
+                },
+            },
+            price: true,
+            orderDate: true,
+        },
+    });
 
-//     const recentSubscriptionsPromise = prisma.subscription.findMany({
-//         take: 4,
-//         orderBy: {
-//             startDate: "desc",
-//         },
-//         select: {
-//             user: {
-//                 select: {
-//                     name: true,
-//                     email: true,
-//                     image: true,
-//                 },
-//             },
-//             price: true,
-//             startDate: true,
-//         },
-//     });
+    const recentSubscriptionsPromise = prisma.subscription.findMany({
+        take: 4,
+        orderBy: {
+            startDate: "desc",
+        },
+        select: {
+            user: {
+                select: {
+                    name: true,
+                    email: true,
+                    image: true,
+                },
+            },
+            price: true,
+            startDate: true,
+        },
+    });
 
-//     // run all promises in parallel so that they don't block each other
-//     const [totalRevenueResult, totalSales, totalSubscriptions, recentSales, recentSubscriptions] = await Promise.all([
-//         totalRevenuePromise,
-//         totalSalesPromise,
-//         totalSubscriptionsPromise,
-//         recentSalesPromise,
-//         recentSubscriptionsPromise,
-//     ]);
+    // run all promises in parallel so that they don't block each other
+    const [totalRevenueResult, totalSales, totalSubscriptions, recentSales, recentSubscriptions] = await Promise.all([
+        totalRevenuePromise,
+        totalSalesPromise,
+        totalSubscriptionsPromise,
+        recentSalesPromise,
+        recentSubscriptionsPromise,
+    ]);
 
-//     const totalRevenue = (totalRevenueResult[0]._sum.price || 0) + (totalRevenueResult[1]._sum.price || 0);
+    const totalRevenue = (totalRevenueResult[0]._sum.price || 0) + (totalRevenueResult[1]._sum.price || 0);
 
-//     return {
-//         totalRevenue: centsToDollars(totalRevenue),
-//         totalSales,
-//         totalSubscriptions,
-//         recentSales,
-//         recentSubscriptions,
-//     };
-// }
+    return {
+        totalRevenue: centsToDollars(totalRevenue),
+        totalSales,
+        totalSubscriptions,
+        recentSales,
+        recentSubscriptions,
+    };
+}
 
 async function checkIfAdmin() {
     const { getUser } = getKindeServerSession();
